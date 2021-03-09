915becde3e79104d9ec1e2d8d5b4d2f2e9f14926
notGonim <mhmodgonim96@gmail.com>
2021-03-09 11:56:47 +0200

    ADDED the error handler middleware

---
+++ server/middlewares/error.js
@@ -0,0 +1,21 @@
+import ErrorResponse from "../utils/error-handling"
+
+
+
+export const errorHandler = (err, req, res, next) => {
+    let error = { ...err }
+    error.message = err.message
+    if (err.code === 11000) {
+        const message = "Duplicated Field value"
+        error = new ErrorResponse(message, 400)
+    }
+    if (err.name === "ValidationError") {
+        const message = Object.values(err.errors).map(val => val.message)
+        error = new ErrorResponse(message, 400)
+
+    }
+    res.status(error.statusCode || 500).json({
+        success: false,
+        error: error.message || 'Server Error'
+    })
+}
